71a3804cd8ba0cbe556a44b08cedc1254de05bc9
Adrian Stokwisz <stokwiszadrian@gmail.com>
2021-11-19 13:08:22 +0100

    zmiana struktury

--- zad01/tests.py
+++ zad01/tests.py
@@ -1,21 +1,6 @@
 import unittest
-
-
-class Hamming:
-    def distance(self, a, b):
-        result = 0
-        if len(a) != len(b):
-            raise ValueError("Podane genotypy maja rozne dlugosci")
-        else:
-            for i in range(0, len(a)):
-                if a[i] != b[i]:
-                    result += 1
-            return result
-
-
+from zad01 import Hamming
 hamming = Hamming()
-
-@unittest.skip
 
 
 class HammingTest(unittest.TestCase):
